09e928e030638ce270ce5309252ee943bc10869f
cd12pc <christoffer.dahlen@gmail.com>
2020-10-03 16:13:01 +0200

    Worked on cell distances.c

--- src/cell_distances.c
+++ src/cell_distances.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-
+#include <cell_distances.h>
 
 
 
@@ -10,11 +10,18 @@ int read_block_from_file(int a, float* b, int c, int* d) {
     //READ A BLOCK OF DATA FROM FILE
 }
 
+
+
 // This function probably doens't need to exist
-//Possibly static inline
-int find_distance(float* a, float* b) {
+// Possibly static inline
+int find_distance(float* result, float* input) {
     //d*d = a*a + b*b + c*c
     //Compute 100 * distance using vector instructions
+
+    // __mm512_loadu_ps(* mem_adress)
+    // __mm512_mul_ps(__m512 a, __m512 b)
+    // __mm512_sqrt_ps(__m512 a)
+    // __mm512_sqrt_round_ps(__m512 a, int rounding)
 }
 
 void find_distrution_from_data(
@@ -34,16 +41,18 @@ void find_distrution_from_data(
 
 int find_distrution_in_file(char* file_name) {
 
-    int max_elements = 10;
+    int file_location = 0;
+    
+
     int dist_size = 5000;
+    unsigned long long* distribution = (unsigned long long*) calloc(dist_size, sizeof(unsigned long long));
 
     float trip[3] = {0., 0., 0.};
-    int file_location = 0;
-
+    
+    int max_elements = 10;
     float* data_to_process = (float *) malloc(3*max_elements*sizeof(float));
     int elements_to_process = 0;
 
-    unsigned long long* distribution = (unsigned long long*) calloc(dist_size, sizeof(unsigned long long));
 
     while(read_block_from_file(
                 file_location,
@@ -51,7 +60,7 @@ int find_distrution_in_file(char* file_name) {
                 max_elements,
                 &elements_to_process)) {
         trip[0] = data_to_process[0];
-        trip[2] = data_to_process[1];
+        trip[1] = data_to_process[1];
         trip[2] = data_to_process[2];
 
         find_distrution_from_data(
